02bc28f69b5997cd011749df904f1efccf2a1b6f
bouncingbumble <jordboudreau@gmail.com>
2023-10-09 12:30:48 -0700

    optimize react query for completed and search

--- src/Hooks/ChecklistHooks.js
+++ src/Hooks/ChecklistHooks.js
@@ -21,99 +21,36 @@ export const useCreateChecklistItem = () => {
         mutationFn: createChecklistItem,
 
         onMutate: async ({ item, task }) => {
-            if (task.completedDate === null) {
-                await queryClient.cancelQueries({ queryKey: ['tasks'] })
+            await queryClient.cancelQueries({ queryKey: ['tasks'] })
 
-                const previousTasks = queryClient.getQueryData(['tasks'])
+            const previousTasks = queryClient.getQueryData(['tasks'])
 
-                queryClient.setQueryData(
-                    ['tasks'],
-                    previousTasks.map((t) => {
-                        if (t._id !== task._id) {
-                            return t
-                        } else {
-                            return {
-                                ...task,
-                                checklist: [
-                                    ...task.checklist,
-                                    { ...item, _id: uuidv4() },
-                                ],
-                            }
+            queryClient.setQueryData(
+                ['tasks'],
+                previousTasks.map((t) => {
+                    if (t._id !== task._id) {
+                        return t
+                    } else {
+                        return {
+                            ...task,
+                            checklist: [
+                                ...task.checklist,
+                                { ...item, _id: uuidv4() },
+                            ],
                         }
-                    })
-                )
-
-                return { previousTasks }
-            } else {
-                //COMPLETED
-                await queryClient.cancelQueries({
-                    queryKey: ['completedTasks'],
+                    }
                 })
-                const completedTasks = queryClient.getQueryData([
-                    'completedTasks',
-                ])
+            )
 
-                queryClient.setQueryData(
-                    ['completedTasks'],
-                    completedTasks.map((t) => {
-                        if (t._id !== task._id) {
-                            return t
-                        } else {
-                            return {
-                                ...task,
-                                checklist: [
-                                    ...task.checklist,
-                                    { ...item, _id: uuidv4() },
-                                ],
-                            }
-                        }
-                    })
-                )
-
-                //SEARCH
-                await queryClient.cancelQueries({
-                    queryKey: ['searchTasks'],
-                })
-                let prevSearchTasks = queryClient.getQueriesData([
-                    'searchTasks',
-                ])
-
-                prevSearchTasks = prevSearchTasks[prevSearchTasks.length - 1][1]
-
-                if (prevSearchTasks) {
-                    // Optimistically update to the new value
-                    queryClient.setQueriesData(
-                        ['searchTasks'],
-                        prevSearchTasks.map((t) => {
-                            if (t._id !== task._id) {
-                                return t
-                            } else {
-                                return {
-                                    ...task,
-                                    checklist: [
-                                        ...task.checklist,
-                                        { ...item, _id: uuidv4() },
-                                    ],
-                                }
-                            }
-                        })
-                    )
-                }
-
-                return { completedTasks, prevSearchTasks }
-            }
+            return { previousTasks }
         },
 
         onError: (error, newItem, context) => {
             queryClient.setQueryData(['tasks'], context.previousTasks)
-            queryClient.setQueryData(['completedTasks'], context.completedTasks)
-            queryClient.setQueryData(['searchTasks'], context.prevSearchTasks)
         },
 
         onSettled: () => {
             queryClient.invalidateQueries({ queryKey: ['tasks'] })
-            queryClient.invalidateQueries({ queryKey: ['completedTasks'] })
-            queryClient.invalidateQueries({ queryKey: ['searchTasks'] })
         },
     })
 }
@@ -125,91 +62,36 @@ export const useUpdateChecklistItem = () => {
         mutationFn: updateChecklistItem,
 
         onMutate: async ({ item, task, updates }) => {
-            if (task.completedDate === null) {
-                await queryClient.cancelQueries({ queryKey: ['tasks'] })
-                const previousTasks = queryClient.getQueryData(['tasks'])
-                let newChecklist = [...task.checklist]
-                newChecklist = newChecklist.map((i) => {
-                    if (i._id !== item._id) {
-                        return i
-                    } else {
-                        return { ...i, ...updates }
-                    }
-                })
-                const newTask = { ...task, checklist: newChecklist }
-                queryClient.setQueryData(
-                    ['tasks'],
-                    previousTasks.map((t) => {
-                        if (t._id !== task._id) {
-                            return t
-                        } else {
-                            return newTask
-                        }
-                    })
-                )
-                return { previousTasks, newTask }
-            } else {
-                await queryClient.cancelQueries({
-                    queryKey: ['completedTasks'],
-                })
-                const completedTasks = queryClient.getQueryData([
-                    'completedTasks',
-                ])
-                let newChecklist = [...task.checklist]
-                newChecklist = newChecklist.map((i) => {
-                    if (i._id !== item._id) {
-                        return i
-                    } else {
-                        return { ...i, ...updates }
-                    }
-                })
-                const newTask = { ...task, checklist: newChecklist }
-                queryClient.setQueryData(
-                    ['completedTasks'],
-                    completedTasks.map((t) => {
-                        if (t._id !== task._id) {
-                            return t
-                        } else {
-                            return newTask
-                        }
-                    })
-                )
-
-                await queryClient.cancelQueries({ queryKey: ['searchTasks'] })
-                let prevSearchTasks = queryClient.getQueriesData([
-                    'searchTasks',
-                ])
-
-                prevSearchTasks = prevSearchTasks[prevSearchTasks.length - 1][1]
-
-                if (prevSearchTasks) {
-                    // Optimistically update to the new value
-                    queryClient.setQueriesData(
-                        ['searchTasks'],
-                        prevSearchTasks.map((t) => {
-                            if (t._id !== task._id) {
-                                return t
-                            } else {
-                                return newTask
-                            }
-                        })
-                    )
+            await queryClient.cancelQueries({ queryKey: ['tasks'] })
+            const previousTasks = queryClient.getQueryData(['tasks'])
+            let newChecklist = [...task.checklist]
+            newChecklist = newChecklist.map((i) => {
+                if (i._id !== item._id) {
+                    return i
+                } else {
+                    return { ...i, ...updates }
                 }
-
-                return { completedTasks, newTask, prevSearchTasks }
-            }
+            })
+            const newTask = { ...task, checklist: newChecklist }
+            queryClient.setQueryData(
+                ['tasks'],
+                previousTasks.map((t) => {
+                    if (t._id !== task._id) {
+                        return t
+                    } else {
+                        return newTask
+                    }
+                })
+            )
+            return { previousTasks, newTask }
         },
 
         onError: (error, newTask, context) => {
             queryClient.setQueryData(['tasks'], context.previousTasks)
-            queryClient.setQueryData(['completedTasks'], context.completedTasks)
-            queryClient.setQueryData(['searchTasks'], context.prevSearchTasks)
         },
 
         onSettled: (settled, newTask, context) => {
             queryClient.invalidateQueries({ queryKey: ['tasks'] })
-            queryClient.invalidateQueries({ queryKey: ['completedTasks'] })
-            queryClient.invalidateQueries({ queryKey: ['searchTasks'] })
         },
     })
 }
@@ -221,94 +103,34 @@ export const useDeleteChecklistItem = () => {
         mutationFn: deleteChecklistItem,
 
         onMutate: async ({ item, task }) => {
-            if (task.completedDate === null) {
-                await queryClient.cancelQueries({ queryKey: ['tasks'] })
-                const previousTasks = queryClient.getQueryData(['tasks'])
+            await queryClient.cancelQueries({ queryKey: ['tasks'] })
+            const previousTasks = queryClient.getQueryData(['tasks'])
 
-                let newChecklist = [...task.checklist]
-                newChecklist = newChecklist.filter((i) => i._id !== item._id)
-                const newTask = { ...task, checklist: newChecklist }
+            let newChecklist = [...task.checklist]
+            newChecklist = newChecklist.filter((i) => i._id !== item._id)
+            const newTask = { ...task, checklist: newChecklist }
 
-                queryClient.setQueryData(
-                    ['tasks'],
-                    previousTasks.map((t) => {
-                        if (t._id !== task._id) {
-                            return t
-                        } else {
-                            return newTask
-                        }
-                    })
-                )
-
-                return { previousTasks, newTask }
-            } else {
-                await queryClient.cancelQueries({
-                    queryKey: ['completedTasks'],
+            queryClient.setQueryData(
+                ['tasks'],
+                previousTasks.map((t) => {
+                    if (t._id !== task._id) {
+                        return t
+                    } else {
+                        return newTask
+                    }
                 })
-                const previousTasks = queryClient.getQueryData([
-                    'completedTasks',
-                ])
+            )
 
-                let newChecklist = [...task.checklist]
-                newChecklist = newChecklist.filter((i) => i._id !== item._id)
-                const newTask = { ...task, checklist: newChecklist }
-
-                queryClient.setQueryData(
-                    ['completedTasks'],
-                    previousTasks.map((t) => {
-                        if (t._id !== task._id) {
-                            return t
-                        } else {
-                            return newTask
-                        }
-                    })
-                )
-
-                await queryClient.cancelQueries({
-                    queryKey: ['searchTasks'],
-                })
-                let prevSearchTasks = queryClient.getQueriesData([
-                    'searchTasks',
-                ])
-
-                prevSearchTasks = prevSearchTasks[prevSearchTasks.length - 1][1]
-
-                if (prevSearchTasks) {
-                    // Optimistically update to the new value
-                    queryClient.setQueriesData(
-                        ['searchTasks'],
-                        prevSearchTasks.map((t) => {
-                            if (t._id !== task._id) {
-                                return t
-                            } else {
-                                return newTask
-                            }
-                        })
-                    )
-                }
-
-                return { previousTasks, newTask, prevSearchTasks }
-            }
+            return { previousTasks, newTask }
         },
 
         onError: (error, newTask, context) => {
             queryClient.setQueryData(['tasks'], context.previousTasks)
-            queryClient.setQueryData(['completedTasks'], context.previousTasks)
-            queryClient.setQueryData(
-                ['prevSearchTasks'],
-                context.prevSearchTasks
-            )
         },
 
         onSettled: (settled, newTask, context) => {
             queryClient.invalidateQueries({
                 queryKey: ['tasks'],
-            })
-            queryClient.invalidateQueries({
-                queryKey: ['completedTasks'],
-            })
-            queryClient.invalidateQueries({
-                queryKey: ['prevSearchTasks'],
             })
         },
     })
