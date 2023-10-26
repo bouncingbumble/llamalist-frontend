69d9b9bd9a151326143fea7e85d6e27066dcb393
bouncingbumble <jordboudreau@gmail.com>
2023-10-25 18:07:14 -0700

    add all goals/levels

--- src/GamificationTab/LevelsAndGoals/levels.js
+++ src/GamificationTab/LevelsAndGoals/levels.js
@@ -2,140 +2,111 @@ export default [
     [
         {
             title: () => 'Add three tasks',
-            isCompleted: (user) => (user.tasks.length > 2 ? true : false),
         },
         {
             title: () => 'Complete three tasks',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: () => 'Play the mini game by clicking on the llama',
-            isCompleted: () => {
-                return false
-            },
         },
     ],
     [
         {
             title: () => 'Text in a task',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: () => 'Complete ten tasks',
-            isCompleted: () => {
-                return false
-            },
         },
         {
-            title: (name) =>
-                `Buy ${name} their first accessory in the apple orchard emporium`,
-            isCompleted: () => {
-                return false
-            },
+            title: () => 'Buy your first accessory in the llama emporium',
+        },
+    ],
+    [
+        {
+            title: () => 'Add a a label to a task',
+        },
+        {
+            title: () => 'Use your notes section',
+        },
+        {
+            title: () => 'Create a checklist item',
         },
     ],
     [
         {
             title: () => 'Email in a task',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: () => 'Score over 10,000 in the llama game',
-            isCompleted: () => {
-                return false
-            },
         },
         {
-            title: () => 'Create fifteen tasks',
-            isCompleted: () => {
-                return false
-            },
+            title: () => 'Set a start date',
         },
     ],
     [
         {
             title: () => 'Thow an apple at a friend',
-            isCompleted: () => {
-                return false
-            },
+        },
+        {
+            title: () => 'Create 3 labels',
+        },
+        {
+            title: () => 'Create fifteen tasks',
+        },
+    ],
+    [
+        {
+            title: () => 'Convert a checklist item to a task',
         },
         {
             title: () => 'Get a three day streak',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: (name) => `Feed ${name} an extra apple today`,
-            isCompleted: () => {
-                return false
-            },
         },
     ],
     [
         {
             title: (name) => `Take ${name} to space`,
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: () => 'Complete 25 tasks',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: (name) => `Score over 25,000 on the llama game`,
-            isCompleted: () => {
-                return false
-            },
         },
     ],
     [
         {
             title: (name) => `Buy ${name} another accessory`,
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: () => 'Get a 5 day streak',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: (name) => `Find this week's golden llama`,
-            isCompleted: () => {
-                return false
-            },
+        },
+    ],
+    [
+        {
+            title: (name) => `Complete 25 checklist items`,
+        },
+        {
+            title: (name) => `Find 3 golden llamas all time`,
+        },
+        {
+            title: (name) => `Complete 15 tasks before their due date`,
         },
     ],
     [
         {
             title: (name) => `Score over 50,000 in the llama game`,
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: () => 'Get a 10 day streak',
-            isCompleted: () => {
-                return false
-            },
         },
         {
             title: (name) => `Complete 50 tasks`,
-            isCompleted: () => {
-                return false
-            },
         },
     ],
 ]
