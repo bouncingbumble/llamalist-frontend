2f917454dd7e4327ed4ad7d073bafb2d704c7a4c
nguido <nguido14@apu.edu>
2023-11-07 13:56:09 -0800

    trying different setActive method

--- src/Microsoft/Teams/Auth/TeamsAuth.js
+++ src/Microsoft/Teams/Auth/TeamsAuth.js
@@ -82,7 +82,7 @@ export default function TeamsAuth() {
             })
             if (auth.status === 'complete') {
                 // begin session
-                await setActive({ session: auth.createdSessionId })
+                await window.Clerk.setActive({ session: auth.createdSessionId })
 
                 // save ms user id to llama profile
                 apiCall('PUT', '/settings', {
